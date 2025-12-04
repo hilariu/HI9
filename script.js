1ef91b45adf41b644d5dde69b11033c3ef63416a
hilariu <hilariolenda@gmail.com>
2025-12-04 14:44:18 -0300

    1.0

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+const API_BASE = "http://localhost:3001"; // ajuste se precisar
+
+async function apiGet(path) {
+    const res = await fetch(API_BASE + path);
+    if (!res.ok) throw new Error("Erro GET " + path);
+    return res.json();
+}
+
+async function apiPost(path, body) {
+    const res = await fetch(API_BASE + path, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+    });
+    const data = await res.json().catch(() => ({}));
+    if (!res.ok) throw new Error(data.error || "Erro POST " + path);
+    return data;
+}
+
+async function apiPut(path, body) {
+    const res = await fetch(API_BASE + path, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+    });
+    const data = await res.json().catch(() => ({}));
+    if (!res.ok) throw new Error(data.error || "Erro PUT " + path);
+    return data;
+}
+
+async function apiDelete(path) {
+    const res = await fetch(API_BASE + path, { method: "DELETE" });
+    const data = await res.json().catch(() => ({}));
+    if (!res.ok) throw new Error(data.error || "Erro DELETE " + path);
+    return data;
+}
+
+
 // ------------------------
 // Caixa diário automático
 // ------------------------
